763027b86480768c35da7831ff7530a9b1e77932
astrozyz <108166330+astrozyz@users.noreply.github.com>
2023-01-16 22:23:31 -0800

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const data = {
 		{
 			id: "work-1",
 			thumbnail: "https://cdn.discordapp.com/attachments/1057924910644736010/1064767514384531506/2023-01-16_20-36-29.mp4",
-			name: "Dead Eye",
+			name: "Dead Eye Replica",
 			date: "Jan 10, 2023",
 			description:
 				"This system is heavily based off of the Dead Eye system found in Red Dead Redemption II. The system, when caps lock or middle mouse is clicked, slows down time and allows you to \"paint\" enemies with X's. If you left click or run out of marks (ammo capacity), it will begin to fire your weapon at said marks. ",
@@ -30,199 +30,167 @@ const data = {
 		},
 	],
 };
-const _0x327a61 = _0x547f;
-(function (_0x71a089, _0x132319) {
-	const _0x126b08 = _0x547f,
-		_0x149493 = _0x71a089();
-	while (!![]) {
-		try {
-			const _0x54c201 =
-				(-parseInt(_0x126b08(0x8c)) / 0x1) * (parseInt(_0x126b08(0x83)) / 0x2) +
-				(-parseInt(_0x126b08(0x8b)) / 0x3) * (-parseInt(_0x126b08(0x93)) / 0x4) +
-				(-parseInt(_0x126b08(0x72)) / 0x5) * (parseInt(_0x126b08(0x71)) / 0x6) +
-				parseInt(_0x126b08(0x99)) / 0x7 +
-				(parseInt(_0x126b08(0x9b)) / 0x8) * (-parseInt(_0x126b08(0x8e)) / 0x9) +
-				(-parseInt(_0x126b08(0x85)) / 0xa) * (parseInt(_0x126b08(0x7a)) / 0xb) +
-				(parseInt(_0x126b08(0x7d)) / 0xc) * (parseInt(_0x126b08(0x8a)) / 0xd);
-			if (_0x54c201 === _0x132319) break;
-			else _0x149493["push"](_0x149493["shift"]());
-		} catch (_0x1458c6) {
-			_0x149493["push"](_0x149493["shift"]());
-		}
-	}
-})(_0x4062, 0xc75a8),
-	(document[_0x327a61(0x9c)]("pricing-img")[_0x327a61(0x87)] = data["pricing-img"]),
-	data["work"][_0x327a61(0x97)]((_0x39121e) => {
-		const _0x405c37 = _0x327a61,
-			_0x2c980a = document[_0x405c37(0x9c)](_0x39121e["id"]),
-			_0xa7b731 = _0x2c980a[_0x405c37(0x73)]("work--image")[0x0][_0x405c37(0x77)]("video")[0x0],
-			_0x552dd8 = _0x2c980a["getElementsByClassName"](_0x405c37(0x95))[0x0];
-		(_0x552dd8[_0x405c37(0x77)]("h1")[0x0][_0x405c37(0x90)] = _0x39121e["name"]),
-			(_0x552dd8["getElementsByTagName"]("span")[0x0][_0x405c37(0x90)] = _0x39121e[_0x405c37(0x76)]),
-			(_0x2c980a["getElementsByTagName"]("p")[0x0][_0x405c37(0x90)] = _0x39121e[_0x405c37(0x92)]),
-			(_0xa7b731["src"] = _0x39121e[_0x405c37(0x81)]);
-	});
-const header = document[_0x327a61(0x73)]("heading-section")[0x0],
-	pastWorkSection = document[_0x327a61(0x73)]("past-work-section")[0x0],
-	availabilitySection = document["getElementsByClassName"](_0x327a61(0x7f))[0x0],
-	pricingSection = document["getElementsByClassName"](_0x327a61(0x98))[0x0],
-	termsSection = document[_0x327a61(0x73)](_0x327a61(0x96))[0x0],
-	getSection = document[_0x327a61(0x73)]("get-section")[0x0],
-	btns = document[_0x327a61(0x73)](_0x327a61(0x7e)),
-	btnsM = document["getElementsByClassName"](_0x327a61(0x86)),
-	headerSticky = header["offsetTop"];
-window["onscroll"] = show;
-function show() {
-	const _0x58a49b = _0x327a61;
-	window[_0x58a49b(0x94)] < pastWorkSection["offsetTop"] - 0x64 &&
-		(btns[0x0]["classList"]["add"](_0x58a49b(0x91)), btns[0x1][_0x58a49b(0x82)][_0x58a49b(0x75)](_0x58a49b(0x91))),
-		window["scrollY"] >= pastWorkSection[_0x58a49b(0x70)] - 0x64 &&
-			(btns[0x1]["classList"][_0x58a49b(0x78)](_0x58a49b(0x91)),
-			btns[0x0]["classList"][_0x58a49b(0x75)](_0x58a49b(0x91)),
-			btns[0x2][_0x58a49b(0x82)][_0x58a49b(0x75)](_0x58a49b(0x91))),
-		window["scrollY"] >= availabilitySection["offsetTop"] - 0x64 &&
-			(btns[0x2]["classList"][_0x58a49b(0x78)]("blue-highlight"),
-			btns[0x1][_0x58a49b(0x82)][_0x58a49b(0x75)](_0x58a49b(0x91)),
-			btns[0x3][_0x58a49b(0x82)]["remove"](_0x58a49b(0x91))),
-		window["scrollY"] >= pricingSection["offsetTop"] - 0xa0 &&
-			(btns[0x3][_0x58a49b(0x82)][_0x58a49b(0x78)](_0x58a49b(0x91)),
-			btns[0x2][_0x58a49b(0x82)]["remove"](_0x58a49b(0x91)),
-			btns[0x4][_0x58a49b(0x82)]["remove"](_0x58a49b(0x91))),
-		window[_0x58a49b(0x94)] >= termsSection[_0x58a49b(0x70)] &&
-			(btns[0x4][_0x58a49b(0x82)][_0x58a49b(0x78)]("blue-highlight"),
-			btns[0x3][_0x58a49b(0x82)]["remove"](_0x58a49b(0x91)),
-			btns[0x5]["classList"][_0x58a49b(0x75)](_0x58a49b(0x91))),
-		window[_0x58a49b(0x94)] >= getSection[_0x58a49b(0x70)] &&
-			(btns[0x5][_0x58a49b(0x82)]["add"](_0x58a49b(0x91)), btns[0x4]["classList"]["remove"](_0x58a49b(0x91))),
-		window[_0x58a49b(0x94)] > headerSticky
-			? header[_0x58a49b(0x82)]["add"](_0x58a49b(0x84))
-			: header[_0x58a49b(0x82)][_0x58a49b(0x75)](_0x58a49b(0x84));
-}
-function show() {
-	const _0x340965 = _0x327a61;
-	window[_0x340965(0x94)] < pastWorkSection[_0x340965(0x70)] - 0x64 &&
-		(btnsM[0x0]["classList"][_0x340965(0x78)]("blue-highlight"), btnsM[0x1][_0x340965(0x82)][_0x340965(0x75)]("blue-highlight")),
-		window[_0x340965(0x94)] >= pastWorkSection["offsetTop"] - 0x64 &&
-			(btnsM[0x1]["classList"][_0x340965(0x78)](_0x340965(0x91)),
-			btnsM[0x0][_0x340965(0x82)]["remove"](_0x340965(0x91)),
-			btnsM[0x2][_0x340965(0x82)][_0x340965(0x75)]("blue-highlight")),
-		window[_0x340965(0x94)] >= availabilitySection["offsetTop"] - 0x64 &&
-			(btnsM[0x2][_0x340965(0x82)][_0x340965(0x78)]("blue-highlight"),
-			btnsM[0x1][_0x340965(0x82)][_0x340965(0x75)](_0x340965(0x91)),
-			btnsM[0x3][_0x340965(0x82)]["remove"](_0x340965(0x91))),
-		window[_0x340965(0x94)] >= pricingSection[_0x340965(0x70)] - 0xa0 &&
-			(btnsM[0x3]["classList"][_0x340965(0x78)](_0x340965(0x91)),
-			btnsM[0x2][_0x340965(0x82)][_0x340965(0x75)](_0x340965(0x91)),
-			btnsM[0x4][_0x340965(0x82)][_0x340965(0x75)]("blue-highlight")),
-		window[_0x340965(0x94)] >= termsSection[_0x340965(0x70)] &&
-			(btnsM[0x4]["classList"][_0x340965(0x78)]("blue-highlight"),
-			btnsM[0x3][_0x340965(0x82)][_0x340965(0x75)](_0x340965(0x91)),
-			btnsM[0x5][_0x340965(0x82)][_0x340965(0x75)]("blue-highlight")),
-		window[_0x340965(0x94)] >= getSection[_0x340965(0x70)] &&
-			(btnsM[0x5][_0x340965(0x82)][_0x340965(0x78)](_0x340965(0x91)), btnsM[0x4][_0x340965(0x82)]["remove"]("blue-highlight")),
-		window[_0x340965(0x94)] > headerSticky
-			? header[_0x340965(0x82)][_0x340965(0x78)](_0x340965(0x84))
-			: header[_0x340965(0x82)][_0x340965(0x75)](_0x340965(0x84));
-}
-function _0x547f(_0x39fb7e, _0xe3193a) {
-	const _0x40623e = _0x4062();
-	return (
-		(_0x547f = function (_0x547f93, _0x3c711a) {
-			_0x547f93 = _0x547f93 - 0x70;
-			let _0x2419e2 = _0x40623e[_0x547f93];
-			return _0x2419e2;
-		}),
-		_0x547f(_0x39fb7e, _0xe3193a)
-	);
-}
-function scrollToSection(_0x1701d9, _0x3aa4f4, _0x5ebc64) {
-	const _0x476158 = _0x327a61,
-		_0x18a114 = document[_0x476158(0x73)](_0x1701d9)[0x0],
-		_0x36ce9d = _0x18a114[_0x476158(0x80)]()["top"] + window[_0x476158(0x74)] + -0x96;
-	_0x3aa4f4[_0x476158(0x82)][_0x476158(0x78)](_0x476158(0x91));
-	for (const _0x262839 of btns) {
-		if (_0x262839 == _0x3aa4f4) continue;
-		_0x262839["classList"][_0x476158(0x75)](_0x476158(0x91));
-	}
-	for (const _0x4293bc of btnsM) {
-		if (_0x4293bc == _0x3aa4f4) continue;
-		_0x4293bc["classList"][_0x476158(0x75)](_0x476158(0x91));
-	}
-	window["scrollTo"]({ top: _0x36ce9d, behavior: _0x476158(0x8f) });
-	if (_0x5ebc64) toggleMenu();
-}
-function _0x4062() {
-	const _0x120e5d = [
-		"innerText",
-		"blue-highlight",
-		"description",
-		"5508jzDKGH",
-		"scrollY",
-		"work--heading",
-		"terms-section",
-		"forEach",
-		"pricing-section",
-		"3732162GHGDTL",
-		"close-blur-transition",
-		"8ePDqBz",
-		"getElementById",
-		"offsetTop",
-		"1104690UgKpKC",
-		"5LpPaPO",
-		"getElementsByClassName",
-		"pageYOffset",
-		"remove",
-		"date",
-		"getElementsByTagName",
-		"add",
-		"style",
-		"10307Dfhlsu",
-		"none",
-		"close-menu-transition",
-		"784872OjIKMu",
-		"pc-btn-head",
-		"availability-section",
-		"getBoundingClientRect",
-		"thumbnail",
-		"classList",
-		"497786kdLGxX",
-		"sticky",
-		"3050gYCAoF",
-		"m-btn-head",
-		"src",
-		"show-blur-transition",
-		"inherit",
-		"208sgsXHY",
-		"1503pSMmcD",
-		"1JEAgYe",
-		"display",
-		"6607746llSGMm",
-		"smooth",
-	];
-	_0x4062 = function () {
-		return _0x120e5d;
+
+document.getElementById("pricing-img").src = data["pricing-img"];
+
+data.work.forEach((work) => {
+	const workSelector = document.getElementById(work.id);
+	const workImage = workSelector.getElementsByClassName("work--image")[0].getElementsByTagName("video")[0];
+	const workHeader = workSelector.getElementsByClassName("work--heading")[0];
+
+	workHeader.getElementsByTagName("h1")[0].innerText = work.name;
+	workHeader.getElementsByTagName("span")[0].innerText = work.date;
+	workSelector.getElementsByTagName("p")[0].innerText = work.description;
+
+	workImage.src = work.thumbnail;
+});
+
+const header = document.getElementsByClassName("heading-section")[0];
+const pastWorkSection = document.getElementsByClassName("past-work-section")[0];
+const availabilitySection = document.getElementsByClassName("availability-section")[0];
+const pricingSection = document.getElementsByClassName("pricing-section")[0];
+const termsSection = document.getElementsByClassName("terms-section")[0];
+const getSection = document.getElementsByClassName("get-section")[0];
+
+const btns = document.getElementsByClassName("pc-btn-head");
+const btnsM = document.getElementsByClassName("m-btn-head");
+
+const headerSticky = header.offsetTop;
+
+window.onload = function () {
+	window.onscroll = function () {
+		show();
+		showM();
 	};
-	return _0x4062();
+};
+
+function show() {
+	if (window.scrollY < pastWorkSection.offsetTop - 100) {
+		btns[0].classList.add("blue-highlight");
+		btns[1].classList.remove("blue-highlight");
+	}
+
+	if (window.scrollY >= pastWorkSection.offsetTop - 100) {
+		btns[1].classList.add("blue-highlight");
+		btns[0].classList.remove("blue-highlight");
+		btns[2].classList.remove("blue-highlight");
+	}
+
+	if (window.scrollY >= availabilitySection.offsetTop - 100) {
+		btns[2].classList.add("blue-highlight");
+		btns[1].classList.remove("blue-highlight");
+		btns[3].classList.remove("blue-highlight");
+	}
+
+	if (window.scrollY >= pricingSection.offsetTop - 160) {
+		btns[3].classList.add("blue-highlight");
+		btns[2].classList.remove("blue-highlight");
+		btns[4].classList.remove("blue-highlight");
+		console.log("e");
+	}
+
+	if (window.scrollY >= termsSection.offsetTop - 160) {
+		btns[4].classList.add("blue-highlight");
+		btns[3].classList.remove("blue-highlight");
+		btns[5].classList.remove("blue-highlight");
+	}
+
+	if (window.scrollY >= getSection.offsetTop - 160) {
+		btns[5].classList.add("blue-highlight");
+		btns[4].classList.remove("blue-highlight");
+	}
+
+	if (window.scrollY > headerSticky) {
+		header.classList.add("sticky");
+	} else {
+		header.classList.remove("sticky");
+	}
 }
+
+function showM() {
+	if (window.scrollY < pastWorkSection.offsetTop - 100) {
+		btnsM[0].classList.add("blue-highlight");
+		btnsM[1].classList.remove("blue-highlight");
+	}
+
+	if (window.scrollY >= pastWorkSection.offsetTop - 100) {
+		btnsM[1].classList.add("blue-highlight");
+		btnsM[0].classList.remove("blue-highlight");
+		btnsM[2].classList.remove("blue-highlight");
+	}
+
+	if (window.scrollY >= availabilitySection.offsetTop - 100) {
+		btnsM[2].classList.add("blue-highlight");
+		btnsM[1].classList.remove("blue-highlight");
+		btnsM[3].classList.remove("blue-highlight");
+	}
+
+	if (window.scrollY >= pricingSection.offsetTop - 100) {
+		btnsM[3].classList.add("blue-highlight");
+		btnsM[2].classList.remove("blue-highlight");
+		btnsM[4].classList.remove("blue-highlight");
+	}
+
+	if (window.scrollY >= termsSection.offsetTop - 100) {
+		btnsM[4].classList.add("blue-highlight");
+		btnsM[3].classList.remove("blue-highlight");
+		btnsM[5].classList.remove("blue-highlight");
+	}
+
+	if (window.scrollY >= getSection.offsetTop - 100) {
+		btnsM[5].classList.add("blue-highlight");
+		btnsM[4].classList.remove("blue-highlight");
+	}
+
+	if (window.scrollY > headerSticky) {
+		header.classList.add("sticky");
+	} else {
+		header.classList.remove("sticky");
+	}
+}
+
+function scrollToSection(section, btn, isMobileMenu) {
+	const sectionToScroll = document.getElementsByClassName(section)[0];
+	const sectionY = sectionToScroll.getBoundingClientRect().top + window.pageYOffset + -150;
+
+	window.scrollTo({ top: sectionY, behavior: "smooth" });
+	setTimeout(() => {
+		btn.classList.add("blue-highlight");
+		for (const btnT of btns) {
+			if (btnT == btn) continue;
+			btnT.classList.remove("blue-highlight");
+		}
+
+		for (const btnM of btnsM) {
+			if (btnM == btn) continue;
+			btnM.classList.remove("blue-highlight");
+		}
+	}, 500);
+	if (isMobileMenu) toggleMenu();
+}
+
 function toggleMenu() {
-	const _0x45a0e2 = _0x327a61,
-		_0x2be4c8 = document["getElementsByClassName"]("menu")[0x0],
-		_0x5eff1e = _0x2be4c8[_0x45a0e2(0x79)][_0x45a0e2(0x8d)] === _0x45a0e2(0x89),
-		_0x113173 = document[_0x45a0e2(0x9c)]("menu-bg");
-	!_0x5eff1e
-		? (_0x2be4c8["classList"][_0x45a0e2(0x78)](_0x45a0e2(0x88)),
-		  _0x113173[_0x45a0e2(0x82)][_0x45a0e2(0x78)]("show-menu-transition"),
-		  (_0x2be4c8[_0x45a0e2(0x79)]["display"] = _0x5eff1e ? _0x45a0e2(0x7b) : _0x45a0e2(0x89)),
-		  setTimeout(() => {
-				const _0x326fe3 = _0x45a0e2;
-				_0x2be4c8["classList"][_0x326fe3(0x75)](_0x326fe3(0x88)), _0x113173[_0x326fe3(0x82)][_0x326fe3(0x75)]("show-menu-transition");
-		  }, 0x258))
-		: (_0x2be4c8[_0x45a0e2(0x82)][_0x45a0e2(0x78)]("close-blur-transition"),
-		  _0x113173["classList"][_0x45a0e2(0x78)]("close-menu-transition"),
-		  setTimeout(() => {
-				const _0x1e37c0 = _0x45a0e2;
-				_0x2be4c8[_0x1e37c0(0x82)][_0x1e37c0(0x75)](_0x1e37c0(0x9a)),
-					_0x113173[_0x1e37c0(0x82)]["remove"](_0x1e37c0(0x7c)),
-					(_0x2be4c8["style"][_0x1e37c0(0x8d)] = _0x1e37c0(0x7b));
-		  }, 0xdc));
+	const menu = document.getElementsByClassName("menu")[0];
+	const isOpen = menu.style.display === "inherit";
+
+	const menuBG = document.getElementById("menu-bg");
+	if (!isOpen) {
+		menu.classList.add("show-blur-transition");
+		menuBG.classList.add("show-menu-transition");
+		menu.style.display = isOpen ? "none" : "inherit";
+
+		setTimeout(() => {
+			menu.classList.remove("show-blur-transition");
+			menuBG.classList.remove("show-menu-transition");
+		}, 600);
+	} else {
+		menu.classList.add("close-blur-transition");
+		menuBG.classList.add("close-menu-transition");
+
+		setTimeout(() => {
+			menu.classList.remove("close-blur-transition");
+			menuBG.classList.remove("close-menu-transition");
+			menu.style.display = "none";
+		}, 220);
+	}
 }
